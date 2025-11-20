c096cabc7d77b6977fdcc3c500c8ae27b8a7b145
Hrishi <hrishikeshkulkarni009@gmail.com>
2025-11-21 00:27:25 +0530

    deploy_cahnges

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",   // <-- MUST end with /
+  base: "./",   // <-- MUST end with /
 
   server: {
     host: "::",
